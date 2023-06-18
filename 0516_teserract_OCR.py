4bb9130ccf67ebdd7f539a76f9669d4448ca382a
coco1718 <128272106+coco1718@users.noreply.github.com>
2023-06-18 21:11:53 +0900

    Update 0516_teserract_OCR.py

--- 0516_teserract_OCR.py
+++ 0516_teserract_OCR.py
@@ -1,5 +1,5 @@
 
-# # https://www.youtube.com/watch?v=L8q-KCbXybc
+
 
 import pytesseract
 from PIL import Image
@@ -12,8 +12,3 @@ result = pytesseract.image_to_string(a, lang = 'kor')
 
 print(result)
 
-#C:\Users\user\anaconda3\python.exe C:\Users\user\PycharmProjects\pythonProject\0516_teserract_OCR.py
-#난 한국사람이야
-
-
-#Process finished with exit code 0
